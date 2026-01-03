06b39a73a276afa253a9fc97a6ed37471fbdb02c
J <joao.adsbrasil@gmail.com>
2026-01-03 01:36:03 -0300

    Qyery Params - Route Params - Body Params

--- server.js
+++ server.js
@@ -5,12 +5,17 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios/:goiaba', (req, res) => {
+    console.log(req)
+
   res.send('Olá, rota acessada com sucesso!')
 })
 
 app.post('/usuarios', (req, res) => {
+    console.log(req)
+
   res.send('Usuário criado com sucesso!')
 })
 
