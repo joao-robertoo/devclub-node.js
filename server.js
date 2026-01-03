6262a04b07e86e43b490b4d8e2cb133a05a083ce
J <joao.adsbrasil@gmail.com>
2026-01-02 23:07:52 -0300

    Criando a primeira rota tipo POST

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/usuarios', (req, res) => {
   res.send('Olá, rota acessada com sucesso!')
 })
 
+app.post('/usuarios', (req, res) => {
+  res.send('Usuário criado com sucesso!')
+})
+
 app.listen(3001);
 // req - Requisição
 // res - Resposta
